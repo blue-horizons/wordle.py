10c9bb3aab0dd085ac9b4efeb40457e099ff3cab
blue-horizons <jojotraingeek@gmail.com>
2022-01-28 14:43:08 +0000

    support for non-ansi terminals; minor debugging

--- wordle.py
+++ wordle.py
@@ -11,47 +11,103 @@ wordList = open("wordle.txt","r")
 
 word = random.choice(wordList.readlines())
 
+#ANSI Codes
 #\033[1;40;32m HIGHLIGHT GREEN
 
 #\033[1;40;30m HIGHLIGHT DARK GRAY
 
 #\033[1;40;33m HIGHLIGHT YELLOW
 
+ansiSupport = input("""
+Do you know if your terminal supports ANSI formatting?
+If it does, the text below should be highlighted green.
+If you don't know, put N.
+\033[1;40;32m IF I'M GREEN YOU HAVE ANSI
 
+y/N
+
+> """)
+
+if ansiSupport.lower() == "y":
+    ansiTrue == True
+elif ansiSupport.lower() == "n":
+    ansiTrue == false
 def guessCheck(word, guess):
     if len(guess) == len(word):
+        for x in guess:
+            if guess[x] == word[x]:
+                correctOut += "\u1f7e9" #green square
+                print(correctOut)
+            elif guess[x] in word:
+                correctOut += "\u1f7e8"
+                print(correctOut)
+            else:
+                correctOut += "\u2b1c"
+                print(correctOut)
+        
+        print(correctOut)
+        return correct
+        return correctOut
+
+
+def guessCheckANSI(word, guess):
+    if len(guess) == len(word) and len(guess) == 5:
         for x in guess:
             if guess[x] == word[x]:
                 correct += f"\033[1;40;32m {x}"
                 correctOut += "\u1f7e9" #green square
             elif guess[x] in word:
                 correct += f"\033[1;40;33m {x}"
-                correctOut += "\u1f7e8"
+                correctOut += "\u1f7e8" #yellow square
             else:
                 correct += f"\033[1;40;30 {x}"
-                correctOut += "\u2b1c"
+                correctOut += "\u2b1c" #Grey/White square
 
         return correct
         return correctOut
 
 
-guess1 = input()
-guessCheck(word, guess1)
 
-guess2 = input()
-guessCheck(word, guess2)
 
-guess3 = input()
-guessCheck(word, guess3)
+if ansiTrue == False:
+    guess1 = input()
+    guessCheck(word, guess1)
 
-guess4 = input()
-guessCheck(word, guess4)
+    guess2 = input()
+    guessCheck(word, guess2)
 
-guess5 = input()
-guessCheck(word, guess5)
+    guess3 = input()
+    guessCheck(word, guess3)
 
-guess6 = input()
-guessCheck(word, guess6)
+    guess4 = input()
+    guessCheck(word, guess4)
+
+    guess5 = input()
+    guessCheck(word, guess5)
+
+    guess6 = input()
+    guessCheck(word, guess6)
+
+else:
+    guess1 = input()
+    guessCheckANSI(word, guess1)
+    
+    guess2 = input()
+    guessCheckANSI(word, guess2)
+
+    guess3 = input()
+    guessCheckANSI(word, guess3)
+
+    guess4 = input()
+    guessCheckANSI(word, guess4)
+
+    guess5 = input()
+    guessCheckANSI(word, guess5)
+
+    guess6 = input()
+    guessCheckANSI(word, guess6)
+
+
 
 if guess6 == word:
     print("""SUCCESS!! You have solved the wordle.""")
