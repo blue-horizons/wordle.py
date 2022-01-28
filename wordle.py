21a4c068713739af849c45a342c1d6afc9345098
blue-horizons <66258429+blue-horizons@users.noreply.github.com>
2022-01-28 12:46:20 +0000

    majo edits to wordle.py

--- wordle.py
+++ wordle.py
@@ -15,14 +15,39 @@ word = random.choice(wordList.readlines)
 #\033[1;40;33m HIGHLIGHT YELLOW
 
 
-
-guess1 = input()
-
-def guessCheck()
-    for x in guess1:
-
-        if guess1[x] in word:
-
+def guessCheck(word, guess):
+    for x in guess:
+        if guess[x] == word[x]:
+            correct += f"\033[1;40;32m {x}"
+            correctOut += "\u1f7e9" #green square
+        elif guess[x] in word:
             correct += f"\033[1;40;33m {x}"
-            if guess1[x] == word[x]:
-                correct += f"\033[1;40;32m {x}"
+            correctOut += "\u1f7e8
+        else:
+            correct += f"\033[1;40;30 {x}
+            correctOut += "\u2b1c"
+
+    return correct
+    return correctOu 
+
+    guess1 = input()
+    guessCheck(word, guess1)
+
+    guess2 = input()
+    guessCheck(word, guess2)
+
+    guess3 = input()
+    guessCheck(word, guess3)
+
+    guess4 = input()
+    guessCheck(word, guess4)
+
+    guess5 = input()
+    guessCheck(word, guess5)
+
+    guess6 = input()
+    guessCheck(word, guess6)
+
+if guess == word:
+    print(""""SUCCESS!! You have solved the wordle.""")
+
