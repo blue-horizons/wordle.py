c980c075d9dd6e5ce2a634505ddd0968c2b9040f
Jdcot06 <jdcotugno006@gmail.com>
2022-01-29 12:44:29 +0000

    major debugs to wordle.py (ANSI support finalised)

--- wordle.py
+++ wordle.py
@@ -1,22 +1,29 @@
 #PYTHON - WORDLESS.PY
 
-#setup
+# setup
 
 import random
+import os
 #import pyperclip
 #from datetime import datetime
 
 #date = datetime.now("%d/%m/%Y @ %H:%M")
-wordList = open("wordle.txt","r")
+wordList = open("wordle.txt", "r")
+print("Words Imported")
 
 word = random.choice(wordList.readlines())
+print("Word Chosen")
+# ANSI Codes
+# \033[1;40;32m HIGHLIGHT GREEN
 
-#ANSI Codes
-#\033[1;40;32m HIGHLIGHT GREEN
+# \033[1;40;30m HIGHLIGHT DARK GRAY
 
-#\033[1;40;30m HIGHLIGHT DARK GRAY
+# \033[1;40;33m HIGHLIGHT YELLOW
+ansiTrue = None
 
-#\033[1;40;33m HIGHLIGHT YELLOW
+global correctOut
+global correct
+
 
 ansiSupport = input("""
 Do you know if your terminal supports ANSI formatting?
@@ -30,95 +37,114 @@ y/N
 
 if ansiSupport.lower() == "y":
     ansiTrue == True
+    print("\033[1;40;33m ANSI Colours Enabled")
 elif ansiSupport.lower() == "n":
-    ansiTrue == false
+    ansiTrue == False
+    print("ANSI Colours not Enabled")
+
+
 def guessCheck(word, guess):
+    correctOut = ""
     if len(guess) == len(word):
         for x in guess:
             if guess[x] == word[x]:
-                correctOut += "\u1f7e9" #green square
+                correctOut += "\u1f7e9"  # green square
                 print(correctOut)
             elif guess[x] in word:
                 correctOut += "\u1f7e8"
                 print(correctOut)
             else:
                 correctOut += "\u2b1c"
+
                 print(correctOut)
-        
+
         print(correctOut)
-        return correct
+
         return correctOut
 
 
 def guessCheckANSI(word, guess):
+    correctOut = ""
+    correct = ""
     if len(guess) == len(word) and len(guess) == 5:
         for x in guess:
             if guess[x] == word[x]:
                 correct += f"\033[1;40;32m {x}"
-                correctOut += "\u1f7e9" #green square
+                correctOut += "\u1f7e9"  # green square
             elif guess[x] in word:
                 correct += f"\033[1;40;33m {x}"
-                correctOut += "\u1f7e8" #yellow square
+                correctOut += "\u1f7e8"  # yellow square
             else:
                 correct += f"\033[1;40;30 {x}"
-                correctOut += "\u2b1c" #Grey/White square
+                correctOut += "\u2b1c"  # Grey/White square
 
-        return correct
+        print(correct)
         return correctOut
 
 
-
-
 if ansiTrue == False:
+    print("Wordle - START")
     guess1 = input()
+    os.system('cls')
     guessCheck(word, guess1)
 
     guess2 = input()
+    os.system('cls')
     guessCheck(word, guess2)
 
     guess3 = input()
+    os.system('cls')
     guessCheck(word, guess3)
 
     guess4 = input()
+    os.system('cls')
     guessCheck(word, guess4)
 
     guess5 = input()
+    os.system('cls')
     guessCheck(word, guess5)
 
     guess6 = input()
+    os.system('cls')
     guessCheck(word, guess6)
 
 else:
+    print("\u001b[31mWordle - START")
     guess1 = input()
+    os.system('cls')
     guessCheckANSI(word, guess1)
-    
+
     guess2 = input()
+    os.system('cls')
     guessCheckANSI(word, guess2)
 
     guess3 = input()
+    os.system('cls')
     guessCheckANSI(word, guess3)
 
     guess4 = input()
+    os.system('cls')
     guessCheckANSI(word, guess4)
 
     guess5 = input()
+    os.system('cls')
     guessCheckANSI(word, guess5)
 
     guess6 = input()
+    os.system('cls')
     guessCheckANSI(word, guess6)
-
 
 
 if guess6 == word:
     print("""SUCCESS!! You have solved the wordle.""")
     print(correctOut)
-    
+
     while choice != "Q":
         print("S Share\nN New\nQ Quit")
-        choice  = input("> ").lower()
+        choice = input("> ").lower()
         if choice == "S":
             #print("Copying to clipboard.")
-            #pyperclip.copy(correctOut)
+            # pyperclip.copy(correctOut)
             print("\n" + correctOut)
         elif choice == "N":
             repeat = True
@@ -130,8 +156,3 @@ if guess6 == word:
                 quit()
         else:
             print("Invalid Option")
-        
-            
-    
-
-
