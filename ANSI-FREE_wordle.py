ff128a7f5250ee6b4ffacbc2f45244d7dbd91778
Jdcot06 <jdcotugno006@gmail.com>
2022-01-30 17:19:38 +0000

    debugs

--- ANSI-FREE_wordle.py
+++ ANSI-FREE_wordle.py
@@ -3,25 +3,28 @@ import random
 words = open("wordle.txt", "r")  # open words list
 
 word = random.choice(words.readlines())  # pick random word
-print("12345")
+print("  12345")
 correct = ""
 guesses = 0
 solved = None
 
-while solved != True and guesses <= 6:
-    guess = input()  # input for guesses
-    for x in guess:  # adds correct letters, removes incorrect letters
-        for y in range(0, 4):
-            if guess == word:  # guess == word?
-                correct = word
-                solved = True
-                print("you solved the wordle")
-            elif guess[y] == word[y]:
-                correct = correct + x.upper()
-                guesses += 1
-            elif guess[y] in word:
-                correct = correct + x.lower()
-                guesses += 1
-            else:
-                correct = correct + "-"
-                guesses += 1
+
+
+while solved != True and guesses < 6:
+    guess = input(str(guesses+1) + " ")  # input for guesses
+    print(correct)
+    if guess == word:  # guess == word?
+        correct = word
+        solved = True
+        print("you solved the wordle")
+    elif guess[guesses] == word[guesses]:
+        correct = correct + guess[guesses].upper()
+        guesses += 1
+    elif guess[guesses] in word:
+        correct = correct + guess[guesses].lower()
+        guesses += 1
+    else:
+        correct = correct + "-"
+        guesses += 1
+        
+    
