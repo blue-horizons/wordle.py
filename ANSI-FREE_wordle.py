a0e1d6d10403a9fcbfa2f62e2435e76e6c2c9a6b
Jdcot06 <jojotraingeek@gmail.com>
2022-01-31 18:17:51 +0000

    added alphabet

--- ANSI-FREE_wordle.py
+++ ANSI-FREE_wordle.py
@@ -1,5 +1,8 @@
 import random
 
+global alphabet
+alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+
 words = open("wordle.txt", "r")  # open words list
 
 word = random.choice(words.readlines())  # pick random word
@@ -8,32 +11,36 @@ correct = ""
 guesses = 0
 solved = None
 
+def countLetter(letter,string):
+    for x in string:
+        if string[x] == letter:
+            alphabet = alphabet - letter
 
 
-while solved != True or guesses <= 5:
-    print(correct)
-    correct = ""
-    
-    guess = input(str(guesses+1) + " ")  # input for guesses
-    guesses +=1
-    for x in range(0, len(guess)):
-        if guess.lower() == word.lower():  # guess == word?
-            correct = word
-            print("You Solved The Wordle!!!")
-            solved = True
-        elif guess == "#####":
-            print("DEBUG\n")
-            print(word)
-            guesses = 0
-        else:
-            if guess[x] == word[x] and guess[x] != word[x]:
-                correct = correct + guess[x].upper()
-            
-            elif guess[x] in word:
-                correct = correct + guess[x].lower()
-        
+while solved != True:  # or guesses <= 5:
+    for i in range(0, 5):
+        print(correct)
+        correct = ""
+
+        guess = input(str(guesses+1) + " ")  # input for guesses
+        guesses += 1
+        for x in range(0, len(guess)):
+            if guess.lower() == word.lower():  # guess == word?
+                correct = word
+                print("You Solved The Wordle!!!")
+                solved = True
+            elif guess == "#####":
+                print("DEBUG\n")
+                print(word)
+                guesses = 0
             else:
-                correct = correct + "-"
+                if guess[x] == word[x] and guess[x] != word[x]:
+                    correct = correct + guess[x].upper()
+                    print(alphabet.lower())
+                elif guess[x] in word:
+                    correct = correct + guess[x].lower()
 
-        
-    
+                else:
+                    correct = correct + "-"
+                    alphabet = alphabet - guess[x]
+                    print[x]
