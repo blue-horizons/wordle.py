f6b658a55ad817458798b16801e12fac61e8f1ef
blue-horizons <jojotraingeek@gmail.com>
2022-01-31 12:44:59 +0000

    debugs

--- ANSI-FREE_wordle.py
+++ ANSI-FREE_wordle.py
@@ -13,19 +13,20 @@ solved = None
 while solved != True or guesses <= 5:
     print(correct)
     guess = input(str(guesses+1) + " ")  # input for guesses
-    for x in guess:
+    for x in range(0, len(guess)):
         if guess == word:  # guess == word?
             correct = word
             solved = True
             print("you solved the wordle")
-        elif guess[guesses] == word[guesses]:
-            correct = correct + guess[guesses].upper()
-            guesses += 1
-        elif guess[guesses] in word:
-            correct = correct + guess[guesses].lower()
-            guesses += 1
         else:
-            correct = correct + "-"
-            guesses += 1
+            if guess[x] == word[x]:
+                correct = correct + guess[x].upper()
+            
+            elif guess[x] in word:
+                correct = correct + guess[x].lower()
+        
+            else:
+                correct = correct + "-"
+        guesses += 1
         
     
