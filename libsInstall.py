a557dcf57a2cdddc57f997601882197debefc505
blue-horizons <jojotraingeek@gmail.com>
2022-02-02 08:36:58 +0000

    Changed to libInstall.py

--- libsInstall.py
+++ libsInstall.py
@@ -6,5 +6,6 @@ subprocess.check_call([sys.executable, "-m","pip","install","pyperclip"])
 
 reqs = subprocess.check_output([sys.executable, "-m","pip","freeze"])
 installed_packages = [r.decode().split("==")[0] for r in reqs.split()]
-
-print(installed_packages + "\nHave been installed")
+print("The following packages:")
+print(installed_packages)
+print("\bHave been installed")
