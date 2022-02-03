38662bb1e94c354ee8782d621fd7adf8df95c5f1
Rory <60672202+JustAnother-Programmer@users.noreply.github.com>
2022-02-03 13:12:30 +0000

    Fixes and improvements
    Removed unnecessary array for menu items, patched setDaily so it works, removed unnecessary close from with statements, remove unnecessary global exports and added running as a script check

--- tests/dailySet.py
+++ tests/dailySet.py
@@ -1,22 +1,17 @@
 from datetime import date
 import random
 
-
-
-
-
 today = date.today()
 todayDate = today.strftime("%d/%m/%Y")
 
 with open(".daily.txt", "r+") as g:
-        if g.readline(0) == todayDate:
-            g.close()
-            print("Today's wordle has already been completed")
-        elif g.readlines != todayDate:
-            with open(".wordle.txt") as f:
-                word = random.choice(f.readlines())
-            g.seek(0)
-            g.write(f"{todayDate}\n{word}")
-            g.truncate()
+    if g.readline(0) == todayDate:
+        print("Today's wordle has already been completed")
+    elif g.readlines() != todayDate:
+        with open(".wordle.txt") as f:
+            word = random.choice(f.readlines())
+        g.seek(0)
+        g.write(f"{todayDate}\n{word}")
+        g.truncate()
 
 print(word)
