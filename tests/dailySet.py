b306d936b0d2831d31f3d2f8099bc5faad62c51c
blue-horizons <jojotraingeek@gmail.com>
2022-02-03 08:46:44 +0000

    dailySet tests

--- tests/dailySet.py
+++ tests/dailySet.py
@@ -1,12 +1,20 @@
+from datetime import date
+from random import choice
 
 
 
+
+
+today = date.today()
+todayDate = today.strftime("%d/%m/%Y")
 
 with open(".daily.txt", "r+") as g:
-        if g.readline(0) != todayDate:
-            g.close(g)
+        if g.readline(0) == todayDate:
+            g.close()
             print("Today's wordle has already been completed")
-        elif g.readlines == todayDate:
+        elif g.readlines != todayDate:
             with open(".wordle.txt") as f:
                 word = random.choice(f.readlines())
             g.write("{todayDate}/n{word}")
+
+print("word")
